01a04e7c1abeef9c298e895f560a6abd0add2099
Melissa Clowdus <clowdusmelissa@gmail.com>
2023-02-05 11:59:01 -0600

    added details and search function

---
+++ src/components/Header/Search/Search.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import './Search.css'
+import axios from 'axios'
+
+function Search({setCharacters}) {
+    //create state to hold search input
+    const [searchValue, setSearchValue] = useState('')
+
+    const handleChange =(e)=>{
+          console.log(e)
+          //store the input value
+          setSearchValue(e.target.value)
+    }
+
+
+    //https://rickandmortyapi.com/api/character/?name=rick
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(searchValue)
+        //I need to call api to get matching characters
+        axios.get(`https://rickandmortyapi.com/api/character/?name=${searchValue}`)
+        .then(res =>{
+          console.log(res.data.results)
+          setCharacters(res.data.results)
+
+          //clear textbox
+          setSearchValue('')
+        })
+        .catch(err => {
+           
+          if (err.response.status === 404) {
+            alert('No Matching Characters')
+          }
+          else {
+            console.log(err) 
+          }
+
+        })
+        
+    }
+
+  return (
+    <form className='search-container'
+      onSubmit = {handleSubmit}>
+      <input type='text'
+             onChange={(e)=>setSearchValue(e.target.value)}
+             value={searchValue}
+             placeholder='Search All Characters'
+             />
+    </form>
+  )
+}
+
+export default Search
---
+++ src/pages/CharacterDetails/CharacterDetails.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react'
+import './CharacterDetails.css'
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+
+function CharacterDetails() {
+    //I need the id from the url
+    const {id} = useParams();
+
+    //create state for character data
+
+    const [character, setCharacter] = useState()
+
+    //https://rickandmortyapi.com/api/character/2
+
+    useEffect (
+        ()=>{
+            //call api to get info for this character
+            axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+            .then(res =>{
+                console.log(res.data)
+                setCharacter(res.data)
+            })
+            .catch(err => console.log(err))
+        }, []
+    )
+
+  return (
+    <div className='details-container'>
+        <img src={character?.image} />
+        <div className='char-info'>
+            <h3>{character?.name}</h3>
+            <ul>
+                <li>{character?.gender}</li>
+                <li>{character?.species}</li>
+                <li>{character?.status}</li>
+            </ul>
+        </div>
+      
+    </div>
+  )
+}
+
+export default CharacterDetails
